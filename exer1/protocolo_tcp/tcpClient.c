55570cde62ae349886642900e489f51cccce2960
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-06-24 15:18:51 -0300

    inserindo retorno do service e verificação de maior e menor número

--- exer1/protocolo_tcp/tcpClient.c
+++ exer1/protocolo_tcp/tcpClient.c
@@ -9,68 +9,84 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <math.h>
 
-#define MAX_SIZE 10
+#define MAX_SIZE 10000
 
-int vet[MAX_SIZE];
+double vet[MAX_SIZE];
 
 void tratasinal(int s){
 	if(s == SIGINT){
-		printf(" --> Digite FIM para encerrar conexão!!!\n>");
+		printf(" COMAND0 - Digite FIM para encerrar conexão!!!\n>");
 	}
 }
 
-void creatingVector (){
+void iniciaVetor (){
 	for (int i = 0; i < MAX_SIZE; i++){
-		vet[i] = i;
+		vet[i] = (sqrt((rand() % MAX_SIZE)/2));
 	};
 };
 
 int main(int argc,char * argv[]) {
-	signal(SIGINT,  tratasinal); // Trata o signal ctrl+c
-	struct  sockaddr_in data_server; // contem os dados do server	 	
-	int     sd;          	      // descritor              
-	int     n,k;                  // caracteres lidos do server 
-	char    buf_sen[MAX_SIZE];     // buffer de dados enviados  
-	char    buf_rec[MAX_SIZE];     // buffer de dados recebidos  
-	
-	//confere o numero de argumentos passados para o programa 
+	signal(SIGINT,  tratasinal);
+	struct  sockaddr_in data_server; 
+	int     sd;          	               
+	int     n,k;                  
+	char    buf_sen[MAX_SIZE];     
+	double    buf_rec[MAX_SIZE];     
+
   	if(argc<3)  {
-    	printf("uso correto: %s <ip_do_servidor> <porta_do_servidor>\n", argv[0]);
+    	printf("OBS - uso correto: %s <ip_do_servidor> <porta_do_servidor>\n", argv[0]);
     	exit(1);  
 	}
 
-	memset((char *)&data_server,0,sizeof(data_server)); // limpa estrutura 
-	memset((int *)&vet,0,sizeof(vet));     // limpa o buffer 
+	memset((char *)&data_server,0,sizeof(data_server)); 
+	memset((int *)&vet,0,sizeof(vet));    
 	
-	data_server.sin_family      = AF_INET; // configura o socket a internet
+	data_server.sin_family      = AF_INET; 
 	data_server.sin_addr.s_addr = inet_addr(argv[1]);
 	data_server.sin_port        = htons(atoi(argv[2]));
 
-	creatingVector();
+	iniciaVetor();
 
-	// Cria o socket 
 	sd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sd < 0) {
-		fprintf(stderr, "Criacao do socket falhou!\n");
+		fprintf(stderr, " ERROR - Criacao do socket!\n");
 		exit(1); 
 	}
 
-	// Conecta o socket ao servidor 	
 	if (connect(sd, (struct sockaddr *)&data_server, sizeof(data_server)) < 0) {
-		fprintf(stderr,"Tentativa de conexao falhou!\n");
+		fprintf(stderr,"ERROR - Tentativa de conexao!\n");
 		exit(1); 
 	}
 
 	int count = 0;
-	while (count < 5) {
-		send(sd,&vet,MAX_SIZE * sizeof(int),0); 
-		printf("ENVIANDO PACOTE DE DADOS > %d", count);
+	while (count < 1) {
+		if (send(sd,&vet,MAX_SIZE * sizeof(double), 0) < 0) {
+			puts("ERROR - Envio de dados ao servidor");
+			break;
+		}
+		printf("ENVIO - Pacote de dados -> %d: \n", count);
+		for (int i = 0; i < MAX_SIZE; i++){
+			printf("%.2f |", vet[i]);
+		};
 		printf("\n");
 		count ++;
 	}
+
+	int aux;
 	
-	printf("------- conexão com o servidor encerrada -----\n");
+	if(recv(sd, &buf_rec, sizeof(buf_rec),0) < 0){
+		puts("ERROR - Recebimento de dados do servidor");
+		exit(1);
+	}else{
+		printf("MAIOR ->  %.2f\n", buf_rec[0]);
+		printf("MENOR -> %.2f\n", buf_rec[1]);
+	};
+
+
+	
+	printf("FIM - Conexão com o servidor encerrada\n");
 	close (sd); 
 	return (0);
 } 
--- exer1/protocolo_tcp/tcpServer.c
+++ exer1/protocolo_tcp/tcpServer.c
@@ -9,81 +9,103 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <float.h>
 
-#define QLEN 10      // tamanho da fila de clientes  
-#define MAX_SIZE 10	// tamanho do buffer 
+#define QLEN 10          
+#define MAX_SIZE 10000	
+
+double compara_maior(double *array){
+	double maior = DBL_MIN;
+	double menor = DBL_MAX;
+	for(int i = 0;i<MAX_SIZE;i++){
+		if(array[i] < menor) menor = array[i];
+		if(array[i] > maior) maior = array[i];
+	}
+	return maior;
+};
+
+double compara_menor(double *array){
+	double maior = DBL_MIN;
+	double menor = DBL_MAX;
+	for(int i = 0;i<MAX_SIZE;i++){
+		if(array[i] < menor) menor = array[i];
+		if(array[i] > maior) maior = array[i];
+	}
+	return menor;
+};
 
 void recebe_cliente(int descritor, struct sockaddr_in endereco_cliente)  {
 
-	int buf_rec[MAX_SIZE]; // buffer de dados recebidos
-	int  n; // número de caracteres lidos do cliente
+	double buf_rec[MAX_SIZE]; 
+	int  n; 
+	double res[2];
 
 	int count = 0;
-	while (count < 5) {
-		memset(&buf_rec, 0x0, MAX_SIZE * sizeof(int));
-		n = recv(descritor, &buf_rec, sizeof(buf_rec),0); // recebendo dados da função send do cliente
+	while (count < 1) {
+		memset(&buf_rec, 0x0, MAX_SIZE * sizeof(double));
+		n = recv(descritor, &buf_rec, sizeof(buf_rec),0); 
 		printf("Client - Pactote %d -> ", count);
 		for(int i = 0; i < MAX_SIZE; i++){
-			printf ("%d | ", buf_rec[i]);
+			printf ("%.2f |", buf_rec[i]);
 		}
 		printf("\n");
-	count++;
-   } 
+		count++;
+  	} 
+
+	res[0] = compara_maior(buf_rec);
+    res[1] = compara_menor(buf_rec);
+
+	write(descritor, &res, 2 * sizeof(double));
 	fprintf(stdout, "Encerrando conexao com: %u ...\n\n", ntohs(endereco_cliente.sin_port));
-	close (descritor); // fecha socket
+	close (descritor);
  }
 
 int main(int argc, char *argv[]){
 
-   	struct sockaddr_in endereco_servidor;  // endereco do servidor   
-   	struct sockaddr_in endereco_cliente;   // endereco do cliente    
-   	int    sd, novo_sd;          // socket descriptors 
+   	struct sockaddr_in endereco_servidor;     
+   	struct sockaddr_in endereco_cliente;    
+   	int    sd, novo_sd;      
 	int    pid,n; 
 
     socklen_t alen;
 
-	if (argc < 3) { // Verifica a quantidade de argumentos passados no terminal
+	if (argc < 3) {
 		printf("Digite IP e Porta para este servidor\n");
 		exit(1); 
 	}
 
 
-   memset((char *)&endereco_servidor,0,sizeof(endereco_servidor)); // limpa variavel endereco_servidor    
-   endereco_servidor.sin_family 		= AF_INET;           	// familia TCP/IP   
-   endereco_servidor.sin_addr.s_addr 	= inet_addr(argv[1]); 	// endereco IP      
-   endereco_servidor.sin_port 		= htons(atoi(argv[2])); // PORTA	    
+   memset((char *)&endereco_servidor,0,sizeof(endereco_servidor));     
+   endereco_servidor.sin_family 		= AF_INET;           
+   endereco_servidor.sin_addr.s_addr 	= inet_addr(argv[1]); 	
+   endereco_servidor.sin_port 		= htons(atoi(argv[2])); 	    
 
-   // Cria o socket 
 	sd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sd < 0) {
-		fprintf(stderr, "Falha ao criar socket!\n");
+		fprintf(stderr, "ERROR - Criacao do socket!\n");
 		exit(1); 
 	}
 
-   // liga o socket a porta e ip 
 	if (bind(sd, (struct sockaddr *)&endereco_servidor, sizeof(endereco_servidor)) < 0) {
-		fprintf(stderr,"Ligacao Falhou!\n");
+		fprintf(stderr,"ERROR - Ligacao Falhou!\n");
 		exit(1); 
 	}
 
-   // Ouve a porta 
 	if (listen(sd, QLEN) < 0) {	
-		fprintf(stderr,"Falhou ouvindo porta!\n");
+		fprintf(stderr,"ERROR - Escuta da porta!\n");
 		exit(1); 
 	}
-	printf("Servidor ouvindo no IP %s, na porta %s ...\n\n", argv[1], argv[2]);
+	printf("Conexão - Servidor ouvindo no IP -> %s | Porta -> %s ...\n\n", argv[1], argv[2]);
 
-   	// Aceita conexoes 
 	alen = sizeof(endereco_cliente);
 	for ( ; ; ){
-		 // espera nova conexao de um processo cliente ... 	
 		if ( (novo_sd=accept(sd, (struct sockaddr *)&endereco_cliente, &alen)) < 0) {
-			fprintf(stdout, "Falha na conexao\n");
+			fprintf(stdout, "ERROR - Falha na conexao\n");
 			exit(1); 
 		}
-		if(fork() == 0){ // Cria processos filho para os clientes
-			fprintf(stdout, "%u conectado. PID: %d\n\n", ntohs(endereco_cliente.sin_port), getpid()); 
-			recebe_cliente(novo_sd, endereco_cliente); // Função do comportamento do cliente
+		if(fork() == 0){
+			fprintf(stdout, "Conexão - %u conectado. PID: %d\n\n", ntohs(endereco_cliente.sin_port), getpid()); 
+			recebe_cliente(novo_sd, endereco_cliente); 
 			exit(0); 
 		}
 	} 
