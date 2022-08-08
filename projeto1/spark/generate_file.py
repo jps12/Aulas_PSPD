c8adb6ff823cde1263c1d154bbdf7c00950554bf
Joao Pedro <jps12carvalho@gmail.com>
2022-08-08 08:56:02 -0300

    feat: Adding spark for projeto 1

---
+++ projeto1/spark/generate_file.py
@@ -0,0 +1,13 @@
+from random import choice
+import json
+
+words_qtde=2**20
+
+def generate_words():
+    with open('words.json', 'r') as jf:
+        words = json.load(jf)
+    with open('input.txt', 'w') as fp:
+        for i in range(words_qtde):
+            fp.write(str(choice(words))+'\n')
+
+generate_words()
